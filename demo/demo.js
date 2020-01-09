35c5783f9e7f3fa621817824ef1c8bda2d6ac46e
Jean Lazarou <jean.lazarou@gmail.com>
2020-01-09 20:34:20 +0100

    Add 'onBlur' property

--- demo/demo.js
+++ demo/demo.js
@@ -1,27 +1,57 @@
-var React = require('react')
-var ReactDom = require('react-dom')
-var TimeInput = require('../src/TimeInput')
+import { useState } from "react";
 
-;(function render (value) {
-  ReactDom.render((
-    <TimeInput value={value} onChange={render} defaultValue='12:00:00:000 AM'/>
-  ), document.getElementById('demo-1'))
-})('11:30:00:000 PM')
+var React = require("react");
+var ReactDom = require("react-dom");
+var TimeInput = require("../src/TimeInput");
 
-;(function render (value) {
-  ReactDom.render((
-    <TimeInput value={value} onChange={render} defaultValue='00:00:00:000'/>
-  ), document.getElementById('demo-2'))
-})('11:30:00:000')
+(function render(value) {
+  ReactDom.render(
+    <TimeInput
+      value={value}
+      onChange={render}
+      defaultValue="12:00:00:000 AM"
+    />,
+    document.getElementById("demo-1")
+  );
+})("11:30:00:000 PM");
+(function render(value) {
+  ReactDom.render(
+    <TimeInput value={value} onChange={render} defaultValue="00:00:00:000" />,
+    document.getElementById("demo-2")
+  );
+})("11:30:00:000");
+(function render(value) {
+  ReactDom.render(
+    <TimeInput value={value} onChange={render} />,
+    document.getElementById("demo-3")
+  );
+})("11:30 PM");
+(function render(value) {
+  ReactDom.render(
+    <TimeInput value={value} onChange={render} />,
+    document.getElementById("demo-4")
+  );
+})("11:30");
 
-;(function render (value) {
-  ReactDom.render((
-    <TimeInput value={value} onChange={render}/>
-  ), document.getElementById('demo-3'))
-})('11:30 PM')
+function ValueView({ value, onChange }) {
+  const [current, setCurrent] = useState(value);
 
-;(function render (value) {
-  ReactDom.render((
-    <TimeInput value={value} onChange={render}/>
-  ), document.getElementById('demo-4'))
-})('11:30')
+  return (
+    <React.Fragment>
+      <span>(onBlur event)</span>
+      <TimeInput
+        value={value}
+        onChange={onChange}
+        onBlur={() => setCurrent(value)}
+      />
+      <span>Time is {current}</span>
+    </React.Fragment>
+  );
+}
+
+(function render(value) {
+  ReactDom.render(
+    <ValueView value={value} onChange={render} />,
+    document.getElementById("demo-5")
+  );
+})("07:07");
